59a7015bb14d2d675f5c27bf89dc4356ae61641d
hermann-wenninger <hermann23@vivaldi.net>
2022-02-23 12:23:02 +0100

    put security-key to env
    sicherheitswarnung berücksichtigt

--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -18,13 +18,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ov*m&zy%y=9!)6=1064qn+p*unle@m(a%t(=zz1-m3c*nje^h7'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
