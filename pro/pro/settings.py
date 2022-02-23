d636f8c1b88d090ba6f2b7e68e6fa8393d81fc0b
hermann <hermann23@vivaldi.net>
2022-02-23 14:42:44 +0100

    secrets

--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -18,19 +18,22 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+import os
+import dotenv # <- New
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ov*m&zy%y=9!)6=1064qn+p*unle@m(a%t(=zz1-m3c*nje^h7'
+# Add .env variables anywhere before SECRET_KEY
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ['SECRET_KEY']
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
-# Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
